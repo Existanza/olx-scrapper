275c51ac1f2ff8272e2cbb8f736347f4f6e477ac
Existanza <kraft.sc2@gmail.com>
2020-06-02 14:07:39 +0200

    1st commit

---
+++ scrapper.py
@@ -0,0 +1,24 @@
+import requests
+import bs4
+
+
+def scrap(page):
+    r = requests.get('https://www.olx.pl/praca/warszawa/q-praca/?page=' + str(page))
+    r.raise_for_status()
+    print(len(r.text))
+    return r.text
+
+
+f = open('offers.txt', 'w', encoding='utf8')
+
+for i in range(1, 100):
+    print("page:" + str(i))
+    soup = bs4.BeautifulSoup(scrap(i), 'html.parser')
+    offers = soup.find_all('div', {'class': 'offer-wrapper'})
+    print(len(offers))
+    for j in range(5, len(offers)):
+        print(offers[j].select('strong')[0].string)
+        f.write(offers[j].select('strong')[0].string)
+        f.write('\n')
+
+f.close()
