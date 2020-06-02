091fd7e8dbf85c52ccfe9d78876eb6d63bd52069
Existanza <kraft.sc2@gmail.com>
2020-06-02 14:23:10 +0200

    fixed

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ import bs4
 
 
 def scrap(page):
-    r = requests.get('https://www.olx.pl/praca/warszawa/q-praca/?page=' + str(page))
+    r = requests.get('https://www.olx.pl/praca/warszawa/?page=' + str(page))
     r.raise_for_status()
     print(len(r.text))
     return r.text
@@ -11,14 +11,16 @@ def scrap(page):
 
 f = open('offers.txt', 'w', encoding='utf8')
 
-for i in range(1, 100):
+for i in range(1, 42):
     print("page:" + str(i))
     soup = bs4.BeautifulSoup(scrap(i), 'html.parser')
     offers = soup.find_all('div', {'class': 'offer-wrapper'})
     print(len(offers))
-    for j in range(5, len(offers)):
-        print(offers[j].select('strong')[0].string)
-        f.write(offers[j].select('strong')[0].string)
-        f.write('\n')
+    for j in range(len(offers)):
+        offer_string = str(offers[j])
+        if 'promoted-list' not in offer_string:
+            print(offers[j].select('strong')[0].string)
+            f.write(offers[j].select('strong')[0].string)
+            f.write('\n')
 
 f.close()
